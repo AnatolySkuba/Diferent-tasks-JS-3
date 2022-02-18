feb4675ab6bad778bdf8412b538b405c0ce77943
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-02-18 15:43:31 +0200

    up

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,10 +2,6 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 const myInput = document.querySelector("input#datetime-picker");
-const fp = flatpickr(myInput, {
-    enableTime: true,
-    dateFormat: "Y-m-d H:i",
-});  // flatpickr
 
 const options = {
   enableTime: true,
@@ -14,5 +10,12 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    const currentDate = new Date();
+    if (selectedDates[0] <= currentDate) {window.alert('Please choose a date in the future')}
   },
 };
+
+const fp = flatpickr(myInput, options);
+
+const date = new Date();
+console.log("Date: ", date);
