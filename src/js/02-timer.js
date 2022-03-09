4033b0cdb18fc08885afc491ed9de766f3252866
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-03-09 10:00:50 +0200

    up

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,30 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Report } from 'notiflix/build/notiflix-report-aio';
 
 const myInput = document.querySelector("input#datetime-picker");
+const startBtn = document.querySelector('button[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+const timer = document.querySelector(".timer");
+const fields = document.querySelectorAll(".field");
+const values = document.querySelectorAll(".value");
+startBtn.disabled = true;
+let timerId = null;
+
+timer.style.display = "flex";
+fields.forEach(field => {
+  field.style.flexDirection = "column";
+  field.style.display = "flex";
+  field.style.margin = "20px";
+  field.style.alignItems = "center";
+  field.style.fontWeight = "bold";
+});
+values.forEach(value => {
+  value.style.fontSize = "30px";
+});
 
 const options = {
   enableTime: true,
@@ -9,13 +32,55 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    const currentDate = new Date();
-    if (selectedDates[0] <= currentDate) {window.alert('Please choose a date in the future')}
+    let currentDate = new Date();
+    if (selectedDates[0] <= currentDate) {
+      Report.failure(
+'Please choose a date in the future',
+'',
+'Okay',
+);
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+
+const handleClickStart = () => {
+  timerId = setInterval(() => {
+    currentDate = new Date();
+    const balanceTime = convertMs(selectedDates[0].getTime() - currentDate.getTime());
+    days.textContent = addLeadingZero(balanceTime.days);
+    hours.textContent = addLeadingZero(balanceTime.hours);
+    minutes.textContent = addLeadingZero(balanceTime.minutes);
+    seconds.textContent = addLeadingZero(balanceTime.seconds);    
+  }, 1000);
+  startBtn.removeEventListener("click", handleClickStart);
+};
+
+      startBtn.addEventListener("click", handleClickStart);
+    }
   },
 };
 
 const fp = flatpickr(myInput, options);
 
-const date = new Date();
-console.log("Date: ", date);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, "0");
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,7 +2,39 @@ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    console.log('+', position, delay);
   } else {
+    console.log('-', position, delay);
     // Reject
   }
+}
+
+function repeat(delay, step, amount) {
+  setTimeout(() => {
+  for (let i = 0; i <= amount; i += 1) {
+    console.log(i);
+    
+        
+      if (i < amount) {
+        timerId = setInterval(() => {
+createPromise(i, delay + i * step);
+  }, step); } else {clearInterval(timerId);}
+      
+    };
+    }, delay);
+}
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = event.currentTarget;
+
+  repeat(delay.value, step.value, amount.value);
+
+  event.currentTarget.reset();
 }
