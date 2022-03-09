bc79eb505e7204d811a36be8c1e3988e62d48f6b
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-03-09 14:07:40 +0200

    up

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,27 +1,32 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
     console.log('+', position, delay);
   } else {
+    Notify.failure(`Rejected promise ${position} in ${delay}ms`);
     console.log('-', position, delay);
     // Reject
   }
 }
 
+let promptCounter = 2;
+
 function repeat(delay, step, amount) {
   setTimeout(() => {
-  for (let i = 0; i <= amount; i += 1) {
-    console.log(i);
-    
-        
-      if (i < amount) {
-        timerId = setInterval(() => {
-createPromise(i, delay + i * step);
-  }, step); } else {clearInterval(timerId);}
-      
-    };
-    }, delay);
+    createPromise(1, delay);
+    const intervalId = setInterval(() => {
+      if (promptCounter === Number(amount) + 1) {
+        clearInterval(intervalId);
+        return;
+      }
+      createPromise(promptCounter, Number(delay) + (promptCounter - 1) * step);          
+      promptCounter += 1; 
+    }, step);
+  }, delay);
 }
 
 const form = document.querySelector(".form");
@@ -30,6 +35,7 @@ form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+  promptCounter = 2;
   const {
     elements: { delay, step, amount }
   } = event.currentTarget;
